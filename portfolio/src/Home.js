8835ad861b0ae0acb5c35c6b2a627f5af3752def
Kaylie V <k.verner37@gmail.com>
2020-07-23 15:22:33 -0400

    created second div for contents of page

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App";
 // import { Grommet } from 'grommet';
 import MainBox from "./components/MainBox/MainBox";
+import SecondBox from "./components/SecondBox/SecondBox";
 
 
 function Projects(props) {
     return (
         <div>
         <MainBox/>
-        
+        <SecondBox/>
         </div>
     )
 }
---
+++ portfolio/src/components/SecondBox/SecondBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SecondBox.css";
+
+function SecondBox() {
+
+  return (
+    <div className="SecondBox">
+      <div className="SecondBoxContents">
+    
+    </div>
+    </div>
+  );
+}
+
+export default SecondBox;
