fd18033d022145e2562d1fa141dbbf89a51256a8
Kaylie V <k.verner37@gmail.com>
2020-07-22 23:10:39 -0400

    updating landing page, testing react library grommet

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Projects.js';  
+import About from './About.js'; 
+import { Box, Button, Heading, Grommet } from 'grommet';
+import { Notification } from 'grommet-icons';
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6'
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+ tag='header'
+ direction='row'
+ align='center'
+ justify='between'
+ background='brand'
+ pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+ elevation='medium'
+ style={{ zIndex: '1' }}
+ {...props}
+ />);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Grommet theme={theme} full>
+        <Box fill>
+        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+     <Box flex align='right' justify='center'>
+       <p>Kaylie Verner</p>
+       <p>Full Stack Web Developer</p>
+     </Box>
+   </Box>
+   <AppBar>
+          <Heading level='3' margin='none'>Portfolio</Heading>
+          <Button icon={<Notification/>}></Button>
+          <Route exact path="/" component={Projects} />
+        <Route path="/about" component={About} />
+                             
+            <Link to="/" className="item">Projects</Link>
+            <Link to="/about" className="item">About</Link>
+        </AppBar>
+      
+        </Box>
+        </Grommet>
+    </BrowserRouter>
   );
 }
-
+ 
 export default App;
