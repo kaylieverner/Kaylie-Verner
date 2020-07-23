dab793b12adf80e6fa989d5f2c7eac4f58be26b6
Kaylie V <k.verner37@gmail.com>
2020-07-23 16:25:06 -0400

    update the format of the projects components

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -3,32 +3,50 @@ import Thumbnail from "./Thumbnail";
 
 function Projects(props) {
     return (
-        <div>
-            <h3>Projects</h3>
-            <Thumbnail
+        // <div>
+        //     <h3>Projects</h3>
+        //     <Thumbnail
+        //         link="lit-woodland-41729.herokuapp.com"
+        //         image="http://t3.gstatic.com/images?q=tbn:ANd9GcTIM5UV56ixF-AYKF9dHUlUmSTC1-LAo4MiHemi078zl0Hf8l1LrOY1mljY4i_RKKVW6u_hg-62_a2l1_nH6BA"
+        //         title="Renovation Hunter"
+        //         description="Desktop App"
+        //         github="https://github.com/kaylieverner/Renovation-Hunter"
+        //     />
+
+        //     <Thumbnail
+        //         link="/airbnb"
+        //         image="http://airbnb-image-url.jpg>"
+        //         title="Airbnb Experiences"
+        //         category="Website"
+        //         github=""
+        //     />
+
+        //     <Thumbnail
+        //         link="/photoshop"
+        //         image="http://photoshop-image-url.jpg"
+        //         title="Photoshop Redesign"
+        //         category="Desktop App"
+        //         github=""
+        //     />
+        // </div>
+
+
+<div className="projects-div">
+  <div className="row" >
+    <div className="col">
+    <h3>Projects</h3>
+    </div>
+  </div>
+  <div className="row mb-5">
+  <Thumbnail
                 link="lit-woodland-41729.herokuapp.com"
-                image="http://https://lit-woodland-41729.herokuapp.com/logo.png-image-url.jpg"
+                image="http://t3.gstatic.com/images?q=tbn:ANd9GcTIM5UV56ixF-AYKF9dHUlUmSTC1-LAo4MiHemi078zl0Hf8l1LrOY1mljY4i_RKKVW6u_hg-62_a2l1_nH6BA"
                 title="Renovation Hunter"
-                category="Desktop App"
+                description="Desktop App"
                 github="https://github.com/kaylieverner/Renovation-Hunter"
             />
-
-            <Thumbnail
-                link="/airbnb"
-                image="http://airbnb-image-url.jpg>"
-                title="Airbnb Experiences"
-                category="Website"
-                github=""
-            />
-
-            <Thumbnail
-                link="/photoshop"
-                image="http://photoshop-image-url.jpg"
-                title="Photoshop Redesign"
-                category="Desktop App"
-                github=""
-            />
-        </div>
+  </div>
+</div> 
     )
 }
 
--- portfolio/src/components/Projects/Thumbnail.js
+++ portfolio/src/components/Projects/Thumbnail.js
@@ -5,16 +5,20 @@ import {Link} from 'react-router-dom';
  
 function Thumbnail(props) {
     return (
-      <div className="project">
-        <Link to={props.link}>
-          <div className="project-image">
-            <img src={props.image} alt="Project Image"/>
-          </div>
-          <div className="project-github">{props.github}</div>
-          <div className="project-title">{props.title}</div>
-          <div className="project-category">{props.category}</div>
-        </Link>
+    <div className="col-4 text-center mb-1 project-container">
+      <img src={props.image}
+        height="400" width="400" alt="Card image cap" className="project-image"/>
+      <div className="overlay">
+        <div className="text">
+        <h3>{props.title}</h3>
+          <p>{props.description}</p>
+          <button type="button" className="btn btn-light"><a target="_blank"
+              href={props.github}>Repository</a></button>
+              <button type="button" className="btn btn-light"><a target="_blank"
+              href={props.link}>Link</a></button>
+        </div>
       </div>
+    </div>
     )
   }
  
