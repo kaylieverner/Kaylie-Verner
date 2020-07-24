8f0b4b04de0fee1e89fb01889c755faf867e2af1
Kaylie V <k.verner37@gmail.com>
2020-07-24 16:09:34 -0400

    Updates

---
+++ precache-manifest.9c183c88919aa2939b51ac127c10e1d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296257f747e96e90e4dfacad4616e25b",
+    "url": "/Kaylie-Verner/index.html"
+  },
+  {
+    "revision": "004805c7f2d495e47e62",
+    "url": "/Kaylie-Verner/static/css/main.1afc41c1.chunk.css"
+  },
+  {
+    "revision": "e3b59b4719dfe5520ca6",
+    "url": "/Kaylie-Verner/static/js/2.e6d55a00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Kaylie-Verner/static/js/2.e6d55a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004805c7f2d495e47e62",
+    "url": "/Kaylie-Verner/static/js/main.40d45477.chunk.js"
+  },
+  {
+    "revision": "978e7f6ead2618321a04",
+    "url": "/Kaylie-Verner/static/js/runtime-main.8faaa0e6.js"
+  }
+]);
