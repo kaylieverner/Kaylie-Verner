9802d39b4f0687c65794e37f9acca38e353adee9
Kaylie V <k.verner37@gmail.com>
2020-07-24 16:35:32 -0400

    update to bio

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -10,11 +10,11 @@ function About() {
 
         <div class="media">
           <div class="media-body">
-            Full Stack Web Developer and experienced Technical Success Manager based in NJ. My professional background has included roles in client support and data management. After gaining exposure to the SaaS industry, I solidified my interest to pursue coding. I then enrolled in a full stack web development course and have continued to expand my knowledge ever since.
+            Full Stack Web Developer and experienced Technical Success Manager based in NJ. My professional background has included roles in the business side of IT in client product support and data management. After gaining exposure to the SaaS industry, I solidified my interest to pursue coding. I then enrolled in a full stack web development course and have continued to expand my knowledge ever since.
 
             I have a passion for creating human-centric solutions, understanding the full picture of how applications function, and approaching challenges with an experimental mindset.
 
-            Additional interests that promote my creativity are digital and film photography, hiking, and ceramics.
+            Additional personal interests that promote my creativity are digital and film photography, hiking, and ceramics.
       </div>
           <img class="ml-3" src="https://lh3.googleusercontent.com/pw/ACtC-3c29hG3ScnB2aVwWLHyN6lJXuOhuD_BFtGG-ygwYgOpzf3-lUuHM6zP8sdOfHS9-Hd-tyTigoU3pbvFITOZsgbM2X6qdD6eRnRXGTZ1ZiCkGaOvTBqVnglZiJJEUvjhEBOwh8cu7Z_4vPR0331n7WYHcA=w1913-h1481-no?authuser=0" alt="professional headshot" />
         </div>
