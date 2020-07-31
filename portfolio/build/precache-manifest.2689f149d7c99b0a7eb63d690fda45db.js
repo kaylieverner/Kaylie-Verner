509ff5d5e654b1f5ed737caf75d449aa84c30454
Kaylie V <k.verner37@gmail.com>
2020-07-31 11:29:21 -0400

    pushing build folder for deployments

---
+++ portfolio/build/precache-manifest.2689f149d7c99b0a7eb63d690fda45db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44145619e2ff2970f4a4c01470383c2f",
+    "url": "/Kaylie-Verner/index.html"
+  },
+  {
+    "revision": "1e991ed85f7d011388a8",
+    "url": "/Kaylie-Verner/static/css/main.1afc41c1.chunk.css"
+  },
+  {
+    "revision": "e3b59b4719dfe5520ca6",
+    "url": "/Kaylie-Verner/static/js/2.e6d55a00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Kaylie-Verner/static/js/2.e6d55a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e991ed85f7d011388a8",
+    "url": "/Kaylie-Verner/static/js/main.954ac206.chunk.js"
+  },
+  {
+    "revision": "978e7f6ead2618321a04",
+    "url": "/Kaylie-Verner/static/js/runtime-main.8faaa0e6.js"
+  }
+]);
